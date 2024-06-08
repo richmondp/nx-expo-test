201eea1966280e00bd186a27758daa3a9dbc05a5
Peter Richmond <prichmond.dev@gmail.com>
2024-06-08 11:34:00 +1000

    Initial commit

---
+++ apps/with-ts-config/app.config.ts
@@ -0,0 +1,35 @@
+import { ConfigContext, ExpoConfig } from "@expo/config";
+
+export default ({
+  config,
+}: ConfigContext): ExpoConfig => ({
+  ...config,
+  "name": "WithTsConfig",
+  "slug": "with-ts-config",
+  "version": "1.0.0",
+  "orientation": "portrait",
+  "icon": "./assets/icon.png",
+  "splash": {
+    "image": "./assets/splash.png",
+    "resizeMode": "contain",
+    "backgroundColor": "#ffffff"
+  },
+  "updates": {
+    "fallbackToCacheTimeout": 0
+  },
+  "assetBundlePatterns": ["**/*"],
+  "ios": {
+    "supportsTablet": true
+  },
+  "android": {
+    "adaptiveIcon": {
+      "foregroundImage": "./assets/adaptive-icon.png",
+      "backgroundColor": "#FFFFFF"
+    }
+  },
+  "web": {
+    "favicon": "./assets/favicon.png",
+    "bundler": "metro"
+  },
+  "plugins": []
+});
